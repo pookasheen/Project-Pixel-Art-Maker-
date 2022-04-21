aa2ce2cbd1871c5ec55ab8b0663a38b449a91114
Sheen S <104039984+pookasheen@users.noreply.github.com>
2022-04-21 19:20:59 -0400

    Create designs.js

---
+++ designs.js
@@ -0,0 +1,11 @@
+// Select color input
+// Select size input
+var height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
